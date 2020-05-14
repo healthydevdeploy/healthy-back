d934d8c29c93babd2f81f763ce29aa25d4225ca5
Andres Di Croche <dicrocheandres@gmail.com>
2020-05-13 23:15:31 -0300

    fix errors and add endpoint get cards with search

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UsePipes, ValidationPipe, Query } from '@nestjs/common';
+import { GetCardsFilterDto } from './dto/get-cards.dto';
+import { Card } from './card.entity';
+import { CardsService } from './cards.service';
 
-@Controller('cards')
-export class CardsController {}
+@Controller()
+export class CardsController {
+    constructor(private cardsService: CardsService) { }
+    @Get('v1/cards')
+    @UsePipes(new ValidationPipe({ transform: true }))
+    getCards(@Query() filterDto: GetCardsFilterDto): Promise<Card[]> {
+        return this.cardsService.getCards(filterDto);
+    }
+}
